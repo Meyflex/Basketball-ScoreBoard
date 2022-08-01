125c22662ae563d6e94a9c0217d22fdc499e1b0d
mohamed elmdimegh <98128042+Meyflex@users.noreply.github.com>
2022-08-01 16:01:52 +0200

    scoreboard

---
+++ index.js
@@ -0,0 +1,31 @@
+let scorehome1=0
+let scorehome=document.getElementById("home-score");
+
+let scoreguest1=0
+let scoreguest=document.getElementById("guest-score");
+
+
+function add1home(){
+    scorehome1=scorehome1+1
+    scorehome.textContent=scorehome1
+}
+function add2home(){
+    scorehome1=scorehome1+2
+    scorehome.textContent=scorehome1
+}
+function add3home(){
+    scorehome1=scorehome1+3
+    scorehome.textContent=scorehome1
+}
+function add1guest(){
+    scoreguest1=scoreguest1+1
+    scoreguest.textContent=scoreguest1
+}
+function add2guest(){
+    scoreguest1=scoreguest1+2
+    scoreguest.textContent=scoreguest1
+}
+function add3guest(){
+    scoreguest1=scoreguest1+3
+    scoreguest.textContent=scoreguest1
+}
